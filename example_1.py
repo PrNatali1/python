60de4b3155f0708df7d6a0ae2a1ae6b7d5fc4920
PrNatali <79377175544@ya.ru>
2023-01-27 01:38:29 +0300

    home work

--- example_1.py
+++ example_1.py
@@ -1,1 +1,8 @@
-n = int(input())
+#Найдите сумму цифр трехзначного числа.
+n = int(input('enter the number: '))
+total = 0
+while n > 0:
+    last_dig = n % 10
+    total += last_dig
+    n //= 10
+print(total)
